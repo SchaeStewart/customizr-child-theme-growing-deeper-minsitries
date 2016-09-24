fe3a6a3509168afb53dcc78b44a898dc84e7d8f9
SchaeStewart <SchaeStewart@gmail.com>
2016-09-24 11:27:04 -0400

    Removed Header Image

--- functions.php
+++ functions.php
@@ -12,34 +12,5 @@ function rdc_navbar_wrapper_class()
 {
     return array('navbar-wrapper', 'clearfix', 'span12');
 }
-
-
-//we add the action with priority set to 5 => it will be displayed in first position (before the slider if any)
-add_action('__after_header', 'scs_add_content_after_header', 20);
-function scs_add_content_after_header() {
-
-    if (is_front_page() == TRUE) {
-        ?>
-        <div id="headerImgWrapper">
-            <img id="headerImg"
-                 src="http://www.growingdeeperministries.org/wp-content/uploads/2016/09/tree-roots-and-girl-smaller.png"
-                 width="90%">
-        </div>
-<!--        <style>-->
-<!--            #headerImgWrapper {-->
-<!--                width: 90%;-->
-<!--                height: auto;-->
-<!--                margin: auto;-->
-<!--            }-->
-<!---->
-<!--            #headerImg {-->
-<!--                display: block;-->
-<!--                margin: auto;-->
-<!--                width: 60%;-->
-<!--            }-->
-<!--        </style>-->
-        <?php
-    }
 ?>
-<?php
 }
